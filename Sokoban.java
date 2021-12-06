0e60815e6542c61f49c921c381cd799dfba2b6c1
Dfreaza <82381849+Dfreaza@users.noreply.github.com>
2021-12-06 19:21:27 +0000

    falta as duas ultimas funçoes

--- Sokoban.java
+++ Sokoban.java
@@ -2,17 +2,32 @@ import java.util.Scanner;
 
 public class Sokoban {
     public static void main(String[] args) {
-        int[][] matriz = new int[3][3];
-        int[] lista = null;
-        boolean resultado = positionInGrid(matriz, lista);
-        System.out.println(resultado);
-    
-        Scanner sc = new Scanner(System.in);
-        char r = readOption(sc);
-        System.out.println(r);
 
-        int[][] matriz2 = {{4, 4 ,4, 1, 1, 1, 4, 1},{4, 3, 5, 2, 1, 1, 4, 1},{4, 4 ,4, 1, 2, 3, 4, 1}};
-        print(matriz2, matriz);
+      System.out.println("Welcome to Sokoban!");
+      System.out.println("Consider that Sokoban symbols are represented by the following digits:");
+      System.out.println(" "+"1 - blank  "+"2 - box  "+"3 - goal  "+"4 - wall  "+"5 - player  ");
+      System.out.println("The symbolic representation on the right may help you while playing.");
+
+
+
+        int[][] matriz = new int[3][3];
+        //int[] lista = null;
+       //boolean resultado = positionInGrid(matriz, lista);
+        //System.out.println(resultado);
+    
+        //Scanner sc = new Scanner(System.in);
+        //char r = readOption(sc);
+        //System.out.println(r);
+
+       int[][] matriz2 = {{4, 4 ,4, 1, 1, 1, 4, 1},{4, 3, 5, 2, 1, 1, 4, 1},{4, 4 ,4, 1, 2, 3, 4, 1},
+                          {4, 3, 4, 4, 2, 1, 4, 1},{4, 1, 4, 1, 3, 1, 4, 4},{4, 2, 1, 2, 2, 2, 3, 4},
+                          {4, 1, 1, 1, 3, 1, 1, 4}, {4, 4, 4, 4, 4, 4, 4, 4}};
+       print(matriz2, matriz);
+
+       //int [][] matrizTeste = {{4, 3, 5, 2, 1, 1, 4, 1},{4, 4 ,4, 1, 1, 1, 4, 1},{4, 4 ,4, 1, 1, 1, 4, 1}};
+       //int[][] matriz3 = null;
+       //System.out.println(isValidGrid(matriz3));
+       //System.out.println(ableToMove(matrizTeste, 'D'));
         
     }
 
@@ -133,20 +148,15 @@ public class Sokoban {
 
   public static char readOption(Scanner scan){
 
-    System.out.println("Welcome to Sokoban!");
-    System.out.println("Consider that Sokoban symbols are represented by the following digits:");
-    System.out.println(" "+"1 - blank  "+"2 - box  "+"3 - goal  "+"4 - wall  "+"5 - player  ");
-    System.out.println("The symbolic representation on the right may help you while playing.");
-
     char readOption;
     boolean valido = false;
     do {
         readOption = scan.next().charAt(0);
         valido = isValidDirection(readOption);
-        if(!valido && readOption != 'E'){
+        if(!valido){
           System.out.println("not valid");
         }
-      }while (!valido && readOption != 'E');
+      }while (!valido);
       
       return readOption;  
     }
@@ -162,11 +172,11 @@ public class Sokoban {
       lista[1] = -1;
     }
     else if (direction == 'U'){
-      lista[0] = 1;
+      lista[0] = -1;
       lista[1] = 0;
     }
     else if (direction == 'D'){
-      lista[0] = -1;
+      lista[0] = 1;
       lista[1] = 0;
     }
     return lista;
@@ -176,7 +186,7 @@ public class Sokoban {
   public static int[] getPlayer(int[][] grid){
     int[] posicao = new int[2];
     for(int i = 0; i < grid.length; i++){
-      for(int j = 0; j < grid.length; j++){
+      for(int j = 0; j < grid[i].length; j++){
         if(grid[i][j] == 5){
           posicao[0] = i;
           posicao[1] = j;
@@ -189,7 +199,7 @@ public class Sokoban {
 
   public static boolean isAvailable(int[][] grid, int[] position){
     boolean ocupado = false;
-    if(grid[position[0]][position[1]] != 3){
+    if(grid[position[0]][position[1]] != 3 || grid[position[0]][position[1]] != 1){
       ocupado = true;
     }
     return ocupado;
@@ -214,46 +224,59 @@ public class Sokoban {
 
 
   public static boolean ableToMove(int[][] grid, char direction){
-      boolean resultado = false;
-      /*int [] localDoPlayer = getPlayer(grid);
-      int[] direcao = delta(direction);
-      int localParaMoverHorizontal = localDoPlayer[0] + direcao[0]; 
-      int localParaMoverVertical = localDoPlayer[1] + direcao[1];
-      int direcaoIndicada = grid[localParaMoverHorizontal][localParaMoverVertical];*/
+    boolean resultado = false;
+    
+    int[] localDoPlayer = getPlayer(grid);
+    
+    int[] direcao = delta(direction);
+    
+    int localParaMoverHorizontal = localDoPlayer[1] + direcao[1];
+    
+    int localParaMoverVertical = localDoPlayer[0] + direcao[0];
+    
+    int direcaoIndicada = grid[localParaMoverVertical][localParaMoverHorizontal];
+    
+    int quadriculaDepoisDaCaixa = grid[localParaMoverVertical+direcao[0]][localParaMoverHorizontal+direcao[1]];
       
-      /*a) a posição adjacente à direita (resp., à esquerda, acima, abaixo) existe e não está ocupada, ou seja,
-      contém um número ímpar ou 
-      b) a posição adjacente à direita (resp., à esquerda, acima, abaixo) contém uma caixa (representado pelo dígito 2) mas a posição adjacente à da caixa, na mesma direção, existe e não está ocupada */
-      return resultado;
+    if(direcaoIndicada == 1 || direcaoIndicada == 3){
+        resultado = true;
+      }else if (direcaoIndicada == 2 && (quadriculaDepoisDaCaixa == 1 || quadriculaDepoisDaCaixa == 3)){
+        resultado = true;
+      }
+    return resultado;
 }
 
     public static void move(int[][] grid, int[][] goals, char direction){
-
+      if (ableToMove(grid, direction)){
+          getPlayer(grid);
+      }
     }
 
-
-
-        /**
-     * Imprime os elementos de um vetor de inteiros separados por espacos De
-     * interesse apenas pedagogico. Para imprimir vetor qq devem usar
-     * System.out.println(Arrays.toString(v))
-     *
-     * @param v O vetor a imprimir
-     * @requires {@code v != null}
-     */
-    public static void printArray(int[] v) {
-        for (int i = 0; i < v.length; i++)
-            System.out.print(v[i] + " ");
-        System.out.println();
-    }
 
 
     public static void print(int[][] grid, int[][] goals){
         for (int i = 0; i < grid.length; i++){
-            printArray(grid[i]);
-
+          for (int j = 0; j < grid[i].length; j++){
+            System.out.print(grid[i][j] + " ");
+          }
+          System.out.print(" | ");
+          for (int k = 0; k < grid[i].length; k++){
+            if(grid[i][k] == 1){
+              System.out.print("   ");
+            }else if(grid[i][k] == 2){
+              System.out.print("  B");
+            }else if(grid[i][k] == 3){
+              System.out.print("  G");
+            }else if(grid[i][k]== 4){
+              System.out.print("  -");
+            }else if(grid[i][k] == 5){
+              System.out.print("  P");
             }
-        }
+          }
+        System.out.println();
+            }
+        System.out.println("Select one of the following options: (R)ight, (L)eft, (U)p, (D)own, (E)xit");
+      }
     
 
 
